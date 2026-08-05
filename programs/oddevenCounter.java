852a35b02fa9a3601f9463a4834a4b008c6ec41e
ananya pandey <anany.boss@gmail.com>
2026-08-05 11:46:45 +0530

    welcomemessage-oddevencounter

--- programs/oddevenCounter.java
+++ programs/oddevenCounter.java
@@ -3,16 +3,21 @@ public class oddevenCounter {
     public static void  main(String[] arge){
         int oddcounter=0;
         int evencounter=0;
+
         Scanner sc = new Scanner(System.in);
+	System.out.println("Welcome to my program");
         System.out.println("Enter a number ");
+
         int num = sc.nextInt();
         sc.nextLine();
+
         System.out.printf("Enter %d numbers ",num);
-        for(int i =1; i <=num; i++) {
+        for(int i=1; i <=num; i++) {
             int n = sc.nextInt();
             sc.nextLine();
+
             if (n%2==0){
-                evencounter++;
+                evencounter++; 
             } else {
                 oddcounter++;
             }
